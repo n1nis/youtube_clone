f17c1f9b805c05a6dead2f7bc49025ab58c5feb3
Krzysztof Rucinski <rucinski.krzysztof02@gmail.com>
2023-08-03 14:54:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import Navbar from "./components/Navbar";
@@ -9,16 +9,14 @@ import ChannelDetail from "./components/ChannelDetail";
 import SearchFeed from "./components/SearchFeed";
 
 const App = () => (
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
-      </Routes>
-    </Box>
-  </BrowserRouter>
+  <Box sx={{ backgroundColor: "#000" }}>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Feed />} />
+      <Route path="/video/:id" element={<VideoDetail />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
+      <Route path="/search/:searchTerm" element={<SearchFeed />} />
+    </Routes>
+  </Box>
 );
 export default App;
